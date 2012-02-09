7164af5557966befb75ae87c2656fb992649a281
Dario Rekowski <dario.rekowski@gmx.de>
2012-02-09 12:27:59 +0100

    fehler

--- data/shader/noise.vert
+++ data/shader/noise.vert
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 varying vec3 v_texCoord3D;
 varying vec2 v_texCoord2D;
@@ -24,3 +25,29 @@ void main()
 	
 	gl_FrontColor  = gl_Color;	
 }
+=======
+
+varying vec3 v_texCoord3D;
+varying vec2 v_texCoord2D;
+varying vec4 c_color;
+
+uniform vec3 SphericalCenter;
+uniform float theta;
+const float PI = 3.1415926535;
+
+
+// simple vertex shader
+
+void main()
+{
+	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+	float lon =  -PI      + (gl_Vertex.x*0.5+0.5)*(2.0*PI);
+	float lat = (-PI*0.5) + (gl_Vertex.y*0.5+0.5)*     PI ;
+    float r = cos(lat);
+	//if(lon >= PI) lon -= 0.0001;
+	//if(lat >= PI) lon -= 0.0001;
+	v_texCoord3D = vec3(r*cos(lon), sin(lat), r*sin(lon));
+	
+	gl_FrontColor  = gl_Color;	
+}
+>>>>>>> 8d2f560ed5612c81fd65ab7efae5c3ce9ace1f37
--- import/include/DRIvlc.h
+++ import/include/DRIvlc.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*/*************************************************************************
  *                                                                       *
  * Engine, Engine-Lib interface class for many things,                  *
@@ -58,3 +59,65 @@ private:
 };
 
 #endif //__DR_Ivlc__
+=======
+/*/*************************************************************************
+ *                                                                       *
+ * Engine, Engine-Lib interface class for many things,                  *
+ * implementierung in extern libs                               		 *
+ * Copyright (C) 2009, 20010, 2011 Dario Rekowski.						 *
+ * Email: dariofrodo@gmx.de   Web: www.mathe-programme.de.tt             *
+ *                                                                       *
+ * This program is free software: you can redistribute it and/or modify  *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * any later version.													 *
+ *																		 *
+ * This program is distributed in the hope that it will be useful,		 *
+ * but WITHOUT ANY WARRAN TY; without even the implied warranty of		 *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the		 *
+ * GNU General Public License for more details.							 *
+ *																		 *
+ * You should have received a copy of the GNU General Public License	 *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                       *
+ *************************************************************************/
+
+#ifndef __DR_Ivlc__
+#define __DR_Ivlc__
+
+#define __vlc_DLL_NAME_ "libvlc.dll"
+/*
+Interface zum einfachen abspielen von Videos durch vlc
+TODO: auch sound abspielen
+Autor: Dario Rekowski
+Datum: 11.11.09
+*/
+
+class ENGINE_API DRIvlc
+{
+public:
+    ~DRIvlc() {if(mInitalized) exit();}
+
+    // Zugrifffunktionen
+    static DRIvlc& Instance();
+
+    static inline DRIvlc& getSingleton() {return Instance();}
+    static inline DRIvlc* getSingletonPtr() {return &Instance();}
+
+
+    void exit();
+    DRReturn init();
+
+private:
+    DRIvlc();
+#ifdef _WIN32
+    static HMODULE mDLL;
+#else
+    static void* mDLL;
+#endif
+
+    bool mInitalized;
+};
+
+#endif //__DR_Ivlc__
+>>>>>>> 8d2f560ed5612c81fd65ab7efae5c3ce9ace1f37
